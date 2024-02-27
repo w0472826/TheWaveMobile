650f8d6e5d686fcbbd0fd0e2cd825c0cf3c1f89c
w0472826 <w0472826@nscc.ca>
2024-02-26 23:34:15 -0400

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [image, setImage] = useState(null);
+
+  // Function to choose photo on phone
+  const pickImage = async() => {
+    //Pick photo on phone using expo-image-picker
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4,3],
+      quality: 1
+    });
+    
+    if(!result.canceled){
+      const uri = result.assets[0].uri;
+      console.log('URI: ' + uri);
+      setImage(uri);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The Wave</Text>
+      <Button title="Choose an Image" onPress={pickImage}></Button>
+      {image && <Image source={{uri:image}} sytle={{width:250, height:250, resizeMode:'cover'}}/>}
+      
       <StatusBar style="auto" />
     </View>
   );
