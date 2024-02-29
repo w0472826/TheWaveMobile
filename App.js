0b0f5a3ba74dcd868108ac451172ecd9c8d97e6d
w0472826 <w0472826@nscc.ca>
2024-02-29 08:38:51 -0400

    Work on drop down list

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Pressable, Text, TextInput, Image, ActivityIndicator, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import base64 from 'react-native-base64';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+const host = 'https://nscc-0304263-wordpress-thewavenews.azurewebsites.net';
+const username = 'W0472826';
+const password = 'hKFW fJir Cq0m xDRV 5yrY nd6H'; //api token
+
+function HomeScreen({navigation}){
 
   const [image, setImage] = useState(null);
   const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   // Function to choose photo on phone
   const pickImage = async() => {
@@ -20,33 +30,128 @@ export default function App() {
     
     if(!result.canceled){
       const uri = result.assets[0].uri;
+      // alert(uri);
       console.log('URI: ' + uri);
       setImage(uri);
     }
   }
+
+  // Upload Featured Image to WordPress
+  const uploadPhoto = async () => {
+    const endPoint = host + '/wp-json/wp/v2/media';
+    const fileName = image.split('/').pop();
+    const formData = new FormData();
+ 
+    formData.append('file', {
+      uri: image,
+      name: fileName
+    });
+ 
+    const result = await fetch(endPoint, {
+      method: 'POST',
+      headers: {        
+        'Content-disposition': 'formdata; filename=' + fileName,
+        'Authorization': 'Basic ' + base64.encode(username + ':' + password)
+      },
+      body: formData
+    });
+ 
+    const response = await result.json();
+    const mediaId = response.id;
+   
+    return mediaId;
+  }
+
+  // Create post in WordPress
+  const submitPost = async () =>{
+    // Validate inputs
+    if(!title || !image){
+      alert('Please complete all input fields.');
+
+    } else {
+      
+      setIsLoading(true);
+
+      // Create media Api
+      const mediaId = await uploadPhoto();
+      console.log('Media ID: ' + mediaId);
+
+      // Create Post Api
+      const endPoint = host + '/wp-json/wp/v2/posts';
+     
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('status', 'publish');
+      formData.append('featured_media', mediaId);
+ 
+      // Create the post
+      const result = await fetch(endPoint, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + base64.encode(username + ':' + password)
+        },
+        body: formData
+      });
+ 
+      const response = await result.json();
+
+      if(response.id){
+        alert('Successfully created Post. ID: ' + response.id);
+      } 
+      else {
+        alert('Opps, something went wrong.');
+      }
+
+      setIsLoading(false);
+    }
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>The Wave</Text>
-      <TextInput 
+    <ScrollView>
+      <View style={styles.container}>
+        
+        <TextInput 
           style={ styles.input } 
           placeholder="Title"
           onChangeText={ text => setTitle(text) } 
           defaultValue= {title}/>
       
-      <Button title="Choose an Image" onPress={pickImage}></Button>
-      {image && <Image source={{uri:image}} sytle={{width:250, height:250, resizeMode:'cover'}}/>}
+        <Pressable onPress={pickImage} style={ styles.button}>
+          <Text style={ styles.buttonText}>Choose Image</Text>
+        </Pressable>
+
+        {image && <Image source={{uri: image}} style={{width:250, height:250, resizeMode:'cover'}}/>}
       
-      <StatusBar style="auto" />
-    </View>
+        <Pressable onPress={submitPost} style={ styles.button}>
+          <Text style={ styles.buttonText}>Submit</Text>
+        </Pressable>
+        {isLoading && <ActivityIndicator/>}
+        <StatusBar style="auto" />
+    
+      </View>
+    </ScrollView>
   );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} options={{title: 'The Wave'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  
+
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
   input: {
     height: 40,
