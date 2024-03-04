11452fced33231ec654fefdb3d31624da60b55c1
w0472826 <w0472826@nscc.ca>
2024-03-03 21:52:03 -0400

    Assignment2-Done

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import * as ImagePicker from 'expo-image-picker';
 import {NavigationContainer, StackActions} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import base64 from 'react-native-base64';
-
+import RNPickerSelect from 'react-native-picker-select';
 
 const Stack = createNativeStackNavigator();
 const host = 'https://nscc-0304263-wordpress-thewavenews.azurewebsites.net';
@@ -14,8 +14,26 @@ const password = 'hKFW fJir Cq0m xDRV 5yrY nd6H'; //api token
 
 function HomeScreen({navigation}){
 
+  return(
+  
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/logo.jpg')} 
+        style={styles.logo}
+      />
+      <Pressable style={styles.button} onPress={()=> navigation.navigate('Create')}>
+        <Text style={styles.buttonText}>Add New Post</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function CreateScreen({navigation}){
+
   const [image, setImage] = useState(null);
   const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   // Function to choose photo on phone
@@ -65,7 +83,7 @@ function HomeScreen({navigation}){
   // Create post in WordPress
   const submitPost = async () =>{
     // Validate inputs
-    if(!title || !image){
+    if(!title || !image || !content || !selectedCategory){
       alert('Please complete all input fields.');
 
     } else {
@@ -76,13 +94,35 @@ function HomeScreen({navigation}){
       const mediaId = await uploadPhoto();
       console.log('Media ID: ' + mediaId);
 
+    // Get category ID from label
+    let categoryId;
+    switch (selectedCategory) {
+      case '3':
+        categoryId = '3';
+        break;
+      case '4':
+        categoryId = '4';
+        break;
+      case '5':
+        categoryId = '5';
+        break;
+      case '6':
+        categoryId = '6';
+        break;
+      default:
+        categoryId = '';
+        break;
+    }
+
       // Create Post Api
       const endPoint = host + '/wp-json/wp/v2/posts';
      
       const formData = new FormData();
       formData.append('title', title);
+      formData.append('content', content);
       formData.append('status', 'publish');
       formData.append('featured_media', mediaId);
+      formData.append('categories', categoryId); 
  
       // Create the post
       const result = await fetch(endPoint, {
@@ -96,7 +136,9 @@ function HomeScreen({navigation}){
       const response = await result.json();
 
       if(response.id){
-        alert('Successfully created Post. ID: ' + response.id);
+
+        // Log the link to the newly created post
+        navigation.navigate('Home');
       } 
       else {
         alert('Opps, something went wrong.');
@@ -104,7 +146,6 @@ function HomeScreen({navigation}){
 
       setIsLoading(false);
     }
-
   }
 
   return (
@@ -117,6 +158,24 @@ function HomeScreen({navigation}){
           onChangeText={ text => setTitle(text) } 
           defaultValue= {title}/>
       
+        <TextInput 
+          style={ styles.input } 
+          placeholder="Content"
+          onChangeText={ text => setContent(text) } 
+          defaultValue= {content}/>
+
+        <View style={styles.pickerContainer}>
+        <RNPickerSelect
+            onValueChange={(value) => setSelectedCategory(value)}
+            items={[
+                { label: 'Arts + Music', value: '3' },
+                { label: 'Food + Drink', value: '4' },
+                { label: 'Opinion', value: '5' },
+                { label: 'Events', value: '6' },
+            ]}
+        />
+        </View>
+       
         <Pressable onPress={pickImage} style={ styles.button}>
           <Text style={ styles.buttonText}>Choose Image</Text>
         </Pressable>
@@ -139,19 +198,18 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Home' component={HomeScreen} options={{title: 'The Wave'}}/>
+        <Stack.Screen name='Create' component={CreateScreen} options={{title: 'Add New Post'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
-  
-
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    marginTop: 50
+    marginTop: 25,
+    marginBottom: 10
   },
   input: {
     height: 40,
@@ -167,7 +225,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'slateblue',
+    backgroundColor: 'skyblue',
+    marginTop: 10,
     marginBottom: 10,
   },
   buttonText: {
@@ -175,6 +234,15 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     letterSpacing: 0.25,
     color: 'white',
+  },
+  logo: {
+    width: 250, 
+    height: 250, 
+    resizeMode: 'contain', 
+  },
+  pickerContainer:{
+    width: 300,
+    marginTop: 10,
+    marginBottom: 20,
   }
-
 });
